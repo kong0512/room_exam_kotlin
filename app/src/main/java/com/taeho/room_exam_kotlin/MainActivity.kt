f5f75d3cf4ecd558c321656d25379e6acf7dbfb1
Taeho Kong <kong0512@gmail.com>
2020-08-08 00:52:59 +0900

    DataBinding 적용

--- app/src/main/java/com/taeho/room_exam_kotlin/MainActivity.kt
+++ app/src/main/java/com/taeho/room_exam_kotlin/MainActivity.kt
@@ -3,30 +3,20 @@ package com.taeho.room_exam_kotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
-import androidx.room.Room
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import androidx.databinding.DataBindingUtil
+
+import com.taeho.room_exam_kotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
         val viewModel by viewModels<MainViewModel>()
 
-        viewModel.getAll().observe(this, Observer { todos ->
-            result_text.text = todos.toString()
-        })
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
 
-        add_button.setOnClickListener {
-            lifecycleScope.launch(Dispatchers.IO) {
-                viewModel.insert(Todo(todo_edit.text.toString()))
-            }
-        }
     }
 }
